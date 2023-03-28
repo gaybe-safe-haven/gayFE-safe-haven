89901ccef4fae5ff822616c28bb5aee662ca0f01
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-03-28 16:30:58 -0700

    Add a form component file and style main tag on various Next pages

---
+++ src/app/Components/Form.jsx
@@ -0,0 +1,37 @@
+import styles from "./Form.module.css";
+
+export default function Form() {
+    return (
+      <form className={styles.formContainer}>
+        <h3 className={styles.formHeading}>My Form</h3>
+        <div className={styles.inputContainer}>
+            <label htmlFor="something" >something:&nbsp;</label>
+            <input 
+              type="text" 
+              name="something" 
+              placeholder="something" 
+              className={styles.formInput}
+            />
+        </div>
+        <div className={styles.inputContainer}>
+            <label htmlFor="quickly">quickly:&nbsp;</label>
+            <input 
+              type="text" 
+              name="quickly" 
+              placeholder="quickly" 
+              className={styles.formInput}
+            />
+        </div>
+        <div className={styles.inputContainer}>
+        <label htmlFor="coming">coming:&nbsp;</label>
+            <input 
+              type="text" 
+              name="coming" 
+              placeholder="coming" 
+              className={styles.formInput}
+            />
+        </div>
+        <button className={styles.button}>Yikes</button>
+      </form>    
+    )
+  }
