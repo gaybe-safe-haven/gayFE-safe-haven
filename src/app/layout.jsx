5f0fae4a3a5f2a742fe3f01aad91dff592b986bb
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-03-28 11:19:40 -0700

    Add nav css module file and styling for nav in layout page

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import Link from 'next/link'
+import styles from "./nav.module.css"
 
 export const metadata = {
   title: 'Gaybe Safe Haven',
@@ -11,11 +12,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <nav>
-          <Link href="/about"><button>about</button></Link>
-          <Link href="/list"><button>list</button></Link>
-          <Link href="/add-shelter"><button>add a shelter</button></Link>
-          <Link href="/api"><button>public api</button></Link>
+        <nav className={styles.navContainer}>
+          <Link href="/about"><button className={styles.linkButton}>about</button></Link>
+          <Link href="/list"><button className={styles.linkButton}>list</button></Link>
+          <Link href="/add-shelter"><button className={styles.linkButton}>add a shelter</button></Link>
+          <Link href="/api"><button className={styles.linkButton}>public api</button></Link>
         </nav>
         {children}
       </body>
