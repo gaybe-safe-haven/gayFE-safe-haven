90a6ff224211b36ea071a76a98b7774a929e1a2e
Anna Peterson <reveragb@gmail.com>
2023-03-28 08:12:07 -0600

    Add content to page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,11 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className={styles.main}>
-     
+     <p>LGBTQ+ youth are 120% more likely to experience homelessness.</p>
+     {/* <img></img> */}
+     <p>But not all shelters are safe for queer and trans folks.</p>
+     {/* <img></img> */}
+     <p>We're asking you to help us track the ones that are.'</p>
     </main>
   )
 }
