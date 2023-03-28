9c1d0311965303eebc680136ff5cc1e3ee4d6417
Anna Peterson <reveragb@gmail.com>
2023-03-28 08:24:43 -0600

    Add content to about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,14 @@
 export default function About() {
   return (
-    <p>about</p>
+    <main>
+      <p>In the face of an increasingly hostile political climate towards LGBTQ+ individuals, it's more important than ever to provide safe spaces for homeless LGBTQ+ youth</p>
+      {/* <img></img> */}
+      <p>We wanted to make an app that would let vulnerable youth search for LGBTQ+-friendly shelters across the United States</p>
+      <p>but  a national database of LGBTQ+-friendly shelters didn’t exist!</p>
+      <p>So, we’re building a comprehensive list of LGBTQ+-friendly youth shelters across the United States</p>
+      <p>and we need YOUR HELP!</p>
+      {/* <img></img> */}
+      <p>If you know of a shelter that should be on our list, please add it. And if you've had an experience with a shelter that's already on our list, please rate it to help others make informed decisions. Together, we can create a network of support and protection for queer and trans youth who need it the most.</p>
+    </main>
   )
 }
