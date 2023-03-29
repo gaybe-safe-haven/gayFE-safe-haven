b4cde71df7f7885145487cfaeb8a63dde1818764
Faye Rosenshein <rosefaye.a@gmail.com>
2023-03-29 13:24:48 -0600

    Use apiCalls file

--- src/apiCalls.jsx
+++ src/apiCalls.jsx
@@ -1,6 +1,6 @@
-export default function GetMockData() {
-	return fetch ('https://bcc0d6a3-cbd5-41b7-be05-284d9753c510.mock.pstmn.io/shelters')
-	.then(response => {
-		if (response.ok) {
-			return response.json()}})
+async function GetMockData() {
+	const mockData = await fetch ('https://bcc0d6a3-cbd5-41b7-be05-284d9753c510.mock.pstmn.io/shelters')
+	return mockData.json()
 }
+
+export default {GetMockData}
--- src/app/list/page.jsx
+++ src/app/list/page.jsx
@@ -1,15 +1,13 @@
 import styles from "../page.module.css"
-// import { GetMockData } from "../../apiCalls"
+import { GetMockData } from "../../apiCalls"
 
 const fetchMockData = async () => {
 	const fetchData = await fetch ('https://bcc0d6a3-cbd5-41b7-be05-284d9753c510.mock.pstmn.io/shelters')
 	return fetchData.json()
-	console.log(data)
-	return data
 }
 
 export default async function List() {
-	const mockData = await fetchMockData()
+	const mockData = await GetMockData()
 	console.log(mockData.data)
 	const mappedData = mockData.data.map(data => data.id)
 	return (
