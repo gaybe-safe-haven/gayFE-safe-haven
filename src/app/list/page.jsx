4920610798244d35f3206b8d1754503b374f924b
Faye Rosenshein <rosefaye.a@gmail.com>
2023-03-29 15:38:37 -0600

    Add p tag to each link

--- src/app/list/page.jsx
+++ src/app/list/page.jsx
@@ -6,12 +6,14 @@ export default async function List() {
 	const shelterData = await getShelterData()
 	// console.log(mockData.data)
 	shelterData.data.sort((a,b) => a.attributes.name.localeCompare(b.attributes.name))
-	const mappedData = shelterData.data.map(data => <Link href={`/${data.id}`} key={data.attributes.id} >{data.attributes.name}</Link>)
+	const mappedData = shelterData.data.map(data => <p><Link href={`/${data.id}`} key={data.attributes.id} >{data.attributes.name}</Link></p>)
 	console.log(mappedData)
 	return (
 		<main className={styles.main}>
 			<p>list</p>
-			<p>{mappedData}</p>
+			<div>
+				{mappedData}
+			</div>
 		</main>
 	)
 }
