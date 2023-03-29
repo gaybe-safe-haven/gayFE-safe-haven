f7ff2dabd59689879936710726d097f19395473b
Faye Rosenshein <rosefaye.a@gmail.com>
2023-03-29 15:35:42 -0600

    Add link to each shelter name

--- src/app/list/page.jsx
+++ src/app/list/page.jsx
@@ -1,11 +1,12 @@
 import styles from "../page.module.css"
 import {getShelterData} from "../../apiCalls"
+import Link from "next/link"
 
 export default async function List() {
 	const shelterData = await getShelterData()
 	// console.log(mockData.data)
 	shelterData.data.sort((a,b) => a.attributes.name.localeCompare(b.attributes.name))
-	const mappedData = shelterData.data.map(data => data.attributes.name)
+	const mappedData = shelterData.data.map(data => <Link href={`/${data.id}`} key={data.attributes.id} >{data.attributes.name}</Link>)
 	console.log(mappedData)
 	return (
 		<main className={styles.main}>
