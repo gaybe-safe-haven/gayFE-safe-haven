0af300e2d60b2704d628b351a932dbbc6d8f3de8
Faye Rosenshein <rosefaye.a@gmail.com>
2023-03-29 15:24:55 -0600

    Add try and catch block to apiCalls

--- src/apiCalls.jsx
+++ src/apiCalls.jsx
@@ -1,4 +1,10 @@
 export const getShelterData = async () => {
-	const shelterData = await fetch ('https://bcc0d6a3-cbd5-41b7-be05-284d9753c510.mock.pstmn.io/shelters')
-	return shelterData.json()
+	const url = 'https://bcc0d6a3-cbd5-41b7-be05-284d9753c510.mock.pstmn.io/shelters'
+	try {
+		const shelterData = await fetch (url)
+		return shelterData.json()
+	} catch (error) {
+		console.log(error)
+		return error
+	}
 }
