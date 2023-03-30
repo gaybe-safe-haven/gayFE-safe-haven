5037c244bc714b29f60abcc57d2ad33514faf067
Faye Rosenshein <rosefaye.a@gmail.com>
2023-03-30 13:37:13 -0600

    Fix some issues in list pages

--- src/apiCalls.jsx
+++ src/apiCalls.jsx
@@ -1,7 +1,7 @@
 export const getShelterData = async (path) => {
 	const url = `https://bcc0d6a3-cbd5-41b7-be05-284d9753c510.mock.pstmn.io/${path}`
 	try {
-		const shelterData = await fetch (url)
+		const shelterData = await fetch (url, { cache: "no-store"})
 		return shelterData.json()
 	} catch (error) {
 		console.log(error)
