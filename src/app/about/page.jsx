d684ca4a298e5a6ee6a9128cf778ad507a322bb3
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-03-29 18:59:10 -0700

    Add images to about and main page display

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,19 +1,20 @@
 import Image from "next/image";
 import styles from "../page.module.css";
-import imageStyles from "../styles/images.module.css"
-import hairPaintTeen from "../../../public/hair-paint-teen.jpg"
+import imageStyles from "../styles/images.module.css";
+import hairPaintTeen from "../../../public/hair-paint-teen.jpg";
+import groupTeensPic from "../../../public/group-teens.jpg";
+import { images } from "../../../next.config";
 
 export default function About() {
   return (
     <main className={styles.main}>
       <p className={styles.greenPTagThin}>In the face of an increasingly hostile political climate towards LGBTQ+ individuals, it&apos;s more important than ever to provide safe spaces for homeless LGBTQ+ youth</p>
-      {/* <img src={hairPaintTeen} alt="hair paint teen" className={styles.hairPaintTeen}></img> */}
-      <Image className={imageStyles.hairPaintTeenImage} src={hairPaintTeen} alt="hair paint teen"/>
+      <Image className={imageStyles.hairPaintTeenImage} src={hairPaintTeen} alt="hair paint teen" />
       <p className={styles.greenPTagThin}>We wanted to make an app that would let vulnerable youth search for LGBTQ+-friendly shelters across the United States</p>
       <p className={styles.pinkPTag}>but  a national database of LGBTQ+-friendly shelters didn&apos;t exist!</p>
       <p className={styles.greenPTagThin}>So, we&apos;re building a comprehensive list of LGBTQ+-friendly youth shelters across the United States</p>
       <p className={styles.purplePTag}>and we need YOUR HELP!</p>
-      <img></img>
+      <Image className={imageStyles.groupTeens} src={groupTeensPic} alt="group of teens" />
       <p className={styles.greenPTagThin}>If you know of a shelter that should be on our list, please add it. And if you&apos;ve had an experience with a shelter that&apos;s already on our list, please rate it to help others make informed decisions. Together, we can create a network of support and protection for queer and trans youth who need it the most.</p>
     </main>
   )
