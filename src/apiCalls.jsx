70ecceed87cf2e3d0c410c704e23b7ca844080f5
Anna Peterson <reveragb@gmail.com>
2023-03-31 10:46:33 -0600

    Move POST function to apiCalls file

--- src/apiCalls.jsx
+++ src/apiCalls.jsx
@@ -2,6 +2,24 @@ export const getShelterData = (path) => {
 	const url = `https://bcc0d6a3-cbd5-41b7-be05-284d9753c510.mock.pstmn.io/${path}`
 	return fetch (url, { cache: "no-store"})
 }
+
+
+export const addShelter = (newShelter) => {
+	const postURL = "https://postman-echo.com/shelters"
+	fetch(postURL, {
+		method: 'POST',
+		headers: {'Content-Type': 'application/json'},
+		body: JSON.stringify(newShelter)
+	})
+	.then(response => {
+		if (response.ok) {
+			return response.json()
+		} else {
+			return Promise.reject(response.status)
+		}
+	})
+}
+
 // export const getServerSideProps = async ({ req, res }) => {
 // 	res.setHeader(
 // 		'Cache-Control',
