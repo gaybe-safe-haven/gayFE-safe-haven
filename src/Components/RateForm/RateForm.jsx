5857e1075eb0be4a27a8e7638dc5d06556efab34
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-31 13:59:06 -0400

    Add handleChange functionality

--- src/Components/RateForm/RateForm.jsx
+++ src/Components/RateForm/RateForm.jsx
@@ -3,20 +3,26 @@ import { useState, useEffect } from 'react'
 
 export default function RateForm({ id, submitReview }) {
   const [review, setReview] = useState({
-    cleanliness: 5,
-    safety: 5,
-    staff: 5,
+    cleanliness: '',
+    safety: '',
+    staff: '',
   })
 
   const clearInputs = () => {
     setReview({
-      cleanliness: 5,
-      safety: 5,
-      staff: 5,
+      cleanliness: '',
+      safety: '',
+      staff: '',
     })
   }
-  const handleChange = (e) => {
-    console.log(e.target)
+  const handleChange = (target) => {
+
+    setReview(prevState => {
+      return {
+        ...prevState,
+        [target.name]: [target.value]
+      }
+    })
   }
 
   const handleSubmit = (e) => {
@@ -36,17 +42,17 @@ export default function RateForm({ id, submitReview }) {
     <p className={styles.label}>Staff was LGBTQ friendly</p>
     <div className={styles.rating}>
       <label for="staff"><img src="/flag.png" alt="lgbtq flag icon" className={styles.icon} /></label>
-      <input type="range" id="staff" name="staff" min="0" max="10" value={review.staff}/>
+      <input type="range" id="staff" name="staff" min="0" max="10" value={review.staff} onChange={(e) => handleChange(e.target)} />
     </div>
     <p className={styles.label}>I was physically safe</p>
     <div className={styles.rating}>
       <label for="staff"><img src="/home.png" alt="safe house icon" className={styles.icon} /></label>
-      <input type="range" id="safety" name="safety" min="0" max="10" value={review.safety}/>
+      <input type="range" id="safety" name="safety" min="0" max="10" value={review.safety} onChange={(e) => handleChange(e.target)}/>
     </div>
     <p className={styles.label}>Facility was clean and sanitary</p>
     <div className={styles.rating}>
       <label for="staff"><img src="/mop.png" alt="mop icon" className={styles.icon} /></label>
-      <input type="range" id="clean" name="clean" min="0" max="10" value={review.safety}/>
+      <input type="range" id="cleanliness" name="cleanliness" min="0" max="10" value={review.cleanliness} onChange={(e) => handleChange(e.target)}/>
     </div>
     <p className={styles.label}>Please only submit a rating if you have stayed at this shelter.</p> 
     <p className={styles.label}>This is community-driven information and we trust you to accurately report your experience.</p>
