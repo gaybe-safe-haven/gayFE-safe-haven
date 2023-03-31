96d2caa26f1356705e0859b5709c5445f1d84d64
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-31 09:31:01 -0400

    Add form jsx

--- src/Components/RateForm/RateForm.jsx
+++ src/Components/RateForm/RateForm.jsx
@@ -1,3 +1,18 @@
 export default function RateForm() {
-  <p>put a form here, please</p>
+  return (
+  <section>
+    <div>
+      <input type="range" id="staff" name="staff" min="0" max="10"/>
+      <label for="staff"><img src="/flag.png" alt="lgbtq flag icon"/></label>
+    </div>
+    <div>
+      <input type="range" id="safety" name="safety" min="0" max="10"/>
+      <label for="staff"><img src="/home.png" alt="safe house icon"/></label>
+    </div>
+    <div>
+      <input type="range" id="clean" name="clean" min="0" max="10"/>
+      <label for="staff"><img src="/mop.png" alt="mop icon"/></label>
+    </div>
+  </section>
+  )
 }
