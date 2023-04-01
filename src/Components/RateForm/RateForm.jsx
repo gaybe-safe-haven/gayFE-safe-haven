609cd99bc04a80acdf3bac859ad1a6a2c407f080
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-04-01 14:09:31 -0400

    Add conditional rendering for error or completed form

--- src/Components/RateForm/RateForm.jsx
+++ src/Components/RateForm/RateForm.jsx
@@ -39,7 +39,7 @@ export default function RateForm({ id, reviewed, error, submitReview }) {
   }
   if(reviewed) {
     return (
-      <section>
+      <section className={styles.review}>
         <p>thank you for taking the time to share your experience with us</p>
       </section>
     )
@@ -49,17 +49,17 @@ export default function RateForm({ id, reviewed, error, submitReview }) {
   <form className={styles.review}>
     <p className={styles.label}>Staff was LGBTQ friendly</p>
     <div className={styles.rating}>
-      <label for="staff"><img src="/flag.png" alt="lgbtq flag icon" className={styles.icon} /></label>
+      <label htmlFor="staff"><img src="/flag.png" alt="lgbtq flag icon" className={styles.icon} /></label>
       <input type="range" id="staff" name="staff" min="0" max="10" value={review.staff} onChange={(e) => handleChange(e.target)} />
     </div>
     <p className={styles.label}>I was physically safe</p>
     <div className={styles.rating}>
-      <label for="staff"><img src="/home.png" alt="safe house icon" className={styles.icon} /></label>
+      <label htmlFor="staff"><img src="/home.png" alt="safe house icon" className={styles.icon} /></label>
       <input type="range" id="safety" name="safety" min="0" max="10" value={review.safety} onChange={(e) => handleChange(e.target)}/>
     </div>
     <p className={styles.label}>Facility was clean and sanitary</p>
     <div className={styles.rating}>
-      <label for="staff"><img src="/mop.png" alt="mop icon" className={styles.icon} /></label>
+      <label htmlFor="staff"><img src="/mop.png" alt="mop icon" className={styles.icon} /></label>
       <input type="range" id="cleanliness" name="cleanliness" min="0" max="10" value={review.cleanliness} onChange={(e) => handleChange(e.target)}/>
     </div>
     <p className={styles.label}>Please only submit a rating if you have stayed at this shelter.</p> 
