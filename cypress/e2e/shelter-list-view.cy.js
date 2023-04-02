01f9eed077ada2a80cf74e12206b0053cdc74e48
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-04-02 11:32:27 -0700

    Add cypress open command to json file

--- cypress/e2e/shelter-list-view.cy.js
+++ cypress/e2e/shelter-list-view.cy.js
@@ -2,4 +2,9 @@ describe("User Flow: As a user, I should be able to view a list of all of the su
   beforeEach(() => {
     cy.visit("http://localhost:3000/list");
   });
+
+  it( "Should navigate from the landing page to a list page", () => {
+    cy.get("button#seeListBtn").contains("list").click()
+    .url().should("eq", "http://localhost:3000/list")
+  })
 })
