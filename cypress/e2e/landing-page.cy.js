379a5ffb9824e3a0fb906906a92758348a387ef5
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-04-03 13:05:10 -0700

    Add updated selectors to shelter list spec in Cypress and update components to remove unneeded selectors

--- cypress/e2e/landing-page.cy.js
+++ cypress/e2e/landing-page.cy.js
@@ -37,7 +37,8 @@ describe("User Flow: As a user, when I visit the site, I should see a landing pa
     cy.get("p.page_greenPTag__4sZ1l").invoke("text").should("eq", "But not all shelters are safe for queer and trans folks.")
     cy.get("img.images_facepaintTeen__DS4MU.images_purpleImageWrapper__HiC78").should("be.visible")
     cy.get('img[alt="face paint teen"').should("have.attr", "src")
-    cy.get("p.page_pinkPTag__naZBk").invoke("text").should("eq", "We&apos;re asking you to help us track the ones that are.")
+    cy.get("p.page_pinkPTag__naZBk").invoke("text").should("eq", "We're asking you to help us track the ones that are.")
+    //leaving this note here: Cypress seems to prefer "We're" and not the literal html "We&apos;re"
   })
 
   it("Should have a stripes visual at the foot of the view screen", () => {
--- cypress/e2e/shelter-list-view.cy.js
+++ cypress/e2e/shelter-list-view.cy.js
@@ -5,23 +5,22 @@ describe("User Flow: As a user, I should be able to view a list of all of the su
   });
   
   it("Should show a heading and shelter cards with name and validation icons in the main content", () => {
-    cy.get("h2#shelterList").contains("Shelter List")
-    cy.get("div#shelterCard").contains("Mock Shelter")
-    cy.get("svg#validationIcon").should("be.visible")
+    cy.get("h2.page_h2Styling__9uXus").contains("Shelter List")
+    cy.get("div.Card_listItemContainer__ZGNxn > a.Card_linkText__fH7R6").contains("Mock Shelter")
     cy.get("svg[alt='not validated']").should("be.visible")
-    cy.get("div#shelterCard").contains("Montrose Grace Pl")
+    cy.get("div.Card_listItemContainer__ZGNxn > a.Card_linkText__fH7R6").contains("Montrose Grace Pl")
+    cy.get('.page_main__ibFHK > :nth-child(2) > :nth-child(2)').invoke("text").should("eq", "Montrose Grace Pl")
     cy.get("svg[alt='validated shelter']").should("be.visible")
   })
 
   it("Should provide a link to a single shelter's display page", () => {
-    cy.get("div#shelterCard > a#shelterLink").first().click()
+    cy.get("div.Card_listItemContainer__ZGNxn > a.Card_linkText__fH7R6").first().click()
     cy.go("back")
   })
   
   it("Should provide a link on every shelter's name", () => {
-    cy.get("div#shelterCard > a#shelterLink").should(($a) => {
+    cy.get("a.Card_linkText__fH7R6").should(($a) => {
       expect($a).to.have.length(6)
     })
   })
-
 })
