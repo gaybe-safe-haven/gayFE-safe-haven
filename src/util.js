22397f3b691bc8ed82316db7410e06530659f1ce
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-04-03 19:38:21 -0400

    Add zipcode checking function

--- src/util.js
+++ src/util.js
@@ -1,3 +1,16 @@
 //check if zipcode is 5 characters AND all numbers
+const checkZip = (input) => {
+  console.log(input, input.length)
+  if(input.length !== 5 || 
+    input.split('').find(digit => !typeof digit !== 'number')) {
+      return false
+  }
+  return true
+}
+
 
 //make sure website doesn't start with http
+
+const checkSite = (input) => {
+
+}
