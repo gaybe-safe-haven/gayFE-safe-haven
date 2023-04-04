9cf2961c98b89e5131101cc64b6ea448a89beab2
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-04-04 13:24:53 -0400

    Move some of the typing into fields to the beforeEach to shorted test length

--- cypress/e2e/add-shelter-form.cy.js
+++ cypress/e2e/add-shelter-form.cy.js
@@ -1,6 +1,10 @@
 describe("User Flow: As a user, when I choose to add a shelter to the list, I am taken to a form where I can input all the relevant information, and submit it. The shelter will be added to the existing list", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/add-shelter");
+    cy.get('input[type=text][name="name"]').type("Test Shelter 1")
+    cy.get('input[type=text][name="streetAddress"]').type("123 Test Ave")
+    cy.get('input[type=text][name="city"]').type("Seattle")
+    cy.get('input[type=text][name="state"]').type("WA")
   });
 
   it("Should display text and a form in the main section", () => {
@@ -26,10 +30,7 @@ describe("User Flow: As a user, when I choose to add a shelter to the list, I am
 
   it("Should have a button that will submit the new information when clicked", () => {
     cy.get("form.Form_formContainer__j8djA").within((form) => {
-      cy.get('input[type=text][name="name"]').type("Test Shelter 1")
-      cy.get('input[type=text][name="streetAddress"]').type("123 Test Ave")
-      cy.get('input[type=text][name="city"]').type("Seattle")
-      cy.get('input[type=text][name="state"]').type("WA")
+    
       cy.get('input[type=text][name="zip"]').type("02460")
       cy.get('input[type=text][name="phoneNumber"]').type("1234567890")
       cy.get('input[type=text][name="websiteURL"]')
@@ -50,10 +51,6 @@ describe("User Flow: As a user, when I choose to add a shelter to the list, I am
 
   it("Should have a message that communicates a successful addition when the button is clicked", () => {
     cy.get("form.Form_formContainer__j8djA").within((form) => {
-      cy.get('input[type=text][name="name"]').type("Test Shelter 2")
-      cy.get('input[type=text][name="streetAddress"]').type("123 Elm Street")
-      cy.get('input[type=text][name="city"]').type("Honolulu")
-      cy.get('input[type=text][name="state"]').type("HI")
       cy.get('input[type=text][name="zip"]').type("02460")
       cy.get('input[type=text][name="phoneNumber"]').type("1234567890")
       cy.get('input[type=text][name="websiteURL"]')
@@ -64,19 +61,17 @@ describe("User Flow: As a user, when I choose to add a shelter to the list, I am
 
   it("Should not submit a form with incomplete fields", () => {
     cy.get("button.Form_button__BbaEK").should('be.disabled')
-    cy.get('input[type=text][name="name"]').type("Test Shelter 2")
-    cy.get('input[type=text][name="streetAddress"]').type("123 Elm Street")
-    cy.get('input[type=text][name="city"]').type("Honolulu")
-    cy.get('input[type=text][name="state"]').type("HI")
     cy.get('input[type=text][name="zip"]').type("02460")
+
     cy.get("button.Form_button__BbaEK").should('be.disabled')
     cy.get('input[type=text][name="phoneNumber"]').type("1234567890")
-    cy.get("button.Form_button__BbaEK").should('be.enabled')
 
+    cy.get("button.Form_button__BbaEK").should('be.enabled')
   })
 
+  it("Should disallow a user from submitting an inproperly formatted zipcode", () => {
 
-    //it should display prompt for incorrect zip code
+  })
 
     //it should display prompt for incorrect phone number
 
