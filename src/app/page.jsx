e50312c5a8236dafefa0870a12b1386286da02f4
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-04-04 12:13:17 -0700

    Add flex containers for landing page and media queries for column and box displays

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,20 +4,20 @@ import styles from "./page.module.css";
 import imageStyles from "./styles/images.module.css";
 import makeupTeens from "../../public/makeup-teens.jpg";
 import facepaintTeenPic from "../../public/facepaint-teen.jpg";
-const roboto = Roboto({ 
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  weight: ['100', '300', '400', '700', '900' ]
-})
+
 
 export default function Home() {
   return (
     <main className={styles.main}>
-     <p className={styles.purplePTag}>LGBTQ+ youth are 120% more likely to experience homelessness.</p>
-     <Image className={imageStyles.makeupTeensImage} src={makeupTeens} alt="makeup teens"/>
-     <p className={styles.greenPTag}>But not all shelters are safe for queer and trans folks.</p>
-     <Image className={imageStyles.facepaintTeen} src={facepaintTeenPic} alt="face paint teen" />
-     <p className={styles.pinkPTag}>We&apos;re asking you to help us track the ones that are.</p>
+      <div className={styles.mainContentTop}>
+        <p className={styles.purplePTag}>LGBTQ+ youth are 120% more likely to experience homelessness.</p>
+        <Image className={imageStyles.makeupTeensImage} src={makeupTeens} alt="makeup teens"/>
+        <p className={styles.greenPTag}>But not all shelters are safe for queer and trans folks.</p>
+      </div>
+      <div className={styles.mainContentBottom}>
+        <Image className={imageStyles.facepaintTeen} src={facepaintTeenPic} alt="face paint teen" />
+        <p className={styles.pinkPTag}>We&apos;re asking you to help us track the ones that are.</p>
+      </div>
     </main>
   )
 }
