db919506133f65dceea4e8724a71cbb9a2a66b93
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-04-04 13:10:37 -0400

    Change website to websiteURL in tests

--- cypress/e2e/add-shelter-form.cy.js
+++ cypress/e2e/add-shelter-form.cy.js
@@ -11,13 +11,15 @@ describe("User Flow: As a user, when I choose to add a shelter to the list, I am
   })
 
   it("Should have a form that includes input boxes and a submit button", () => {
+    cy.intercept('POST', 'https://bcc0d6a3-cbd5-41b7-be05-284d9753c510.mock.pstmn.io/shelters',{fixture: '../fixtures/postShelter200.json'} )
     cy.get("form.Form_formContainer__j8djA").within((form) => {
       cy.get('input[type=text][name="name"]')
       cy.get('input[type=text][name="streetAddress"]')
       cy.get('input[type=text][name="city"]')
       cy.get('input[type=text][name="state"]')
+      cy.get('input[type=text][name="zip"]')
       cy.get('input[type=text][name="phoneNumber"]')
-      cy.get('input[type=text][name="website"]')
+      cy.get('input[type=text][name="websiteURL"]')
       cy.get("button.Form_button__BbaEK").contains("Add Shelter")
     }).should("be.visible")
   })
@@ -28,8 +30,9 @@ describe("User Flow: As a user, when I choose to add a shelter to the list, I am
       cy.get('input[type=text][name="streetAddress"]').type("123 Test Ave")
       cy.get('input[type=text][name="city"]').type("Seattle")
       cy.get('input[type=text][name="state"]').type("WA")
+      cy.get('input[type=text][name="zip"]').type("02460")
       cy.get('input[type=text][name="phoneNumber"]').type("1234567890")
-      cy.get('input[type=text][name="website"]')
+      cy.get('input[type=text][name="websiteURL"]')
     })
 
     cy.get("button.Form_button__BbaEK").contains("Add Shelter").click()
@@ -39,8 +42,9 @@ describe("User Flow: As a user, when I choose to add a shelter to the list, I am
       cy.get('input[type=text][name="streetAddress"]').should("be.empty")
       cy.get('input[type=text][name="city"]').should("be.empty")
       cy.get('input[type=text][name="state"]').should("be.empty")
+      cy.get('input[type=text][name="zip"]').should("be.empty")
       cy.get('input[type=text][name="phoneNumber"]').should("be.empty")
-      cy.get('input[type=text][name="website"]').should("be.empty")
+      cy.get('input[type=text][name="websiteURL"]').should("be.empty")
     })
   })
 
@@ -50,10 +54,15 @@ describe("User Flow: As a user, when I choose to add a shelter to the list, I am
       cy.get('input[type=text][name="streetAddress"]').type("123 Elm Street")
       cy.get('input[type=text][name="city"]').type("Honolulu")
       cy.get('input[type=text][name="state"]').type("HI")
+      cy.get('input[type=text][name="zip"]').type("02460")
       cy.get('input[type=text][name="phoneNumber"]').type("1234567890")
-      cy.get('input[type=text][name="website"]')
+      cy.get('input[type=text][name="websiteURL"]')
       cy.get("button.Form_button__BbaEK").contains("Add Shelter").click()
     })
+
+    cy.get("p.message").contains("Your addition was successful!").should("exist")
+  })
+
 
     //it should display prompt for incorrect zip code
 
@@ -66,7 +75,4 @@ describe("User Flow: As a user, when I choose to add a shelter to the list, I am
     //it should display an error message if the user tries to submit a shelter that already exists
 
     //it should display an error message if the zip code entered is invalid
-
-    cy.get("p.message").contains("Your addition was successful!").should("exist")
-  })
 })
