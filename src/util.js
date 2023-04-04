c91914f644177f81841272f14e3ba761596b7dd1
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-04-03 21:53:49 -0400

    Add regex to zip checker

--- src/util.js
+++ src/util.js
@@ -1,14 +1,14 @@
 
 export const checkZip = (input) => {
-  if(input.length !== 5 || 
-    input.split('').find(digit => !typeof digit !== 'number')) {
+  console.log('all num nums?', /^\d+$/.test(input))
+  if(input.length !== 5 || !(/^\d+$/.test(input))) {
       return false
   }
   return true
 }
 
 export const checkSite = (input) => {
-  console.log(input, input.length, typeof input)
+
   if (input.includes('http://') || !input.includes('www.')) {
     return false
   }
