de8c03cded97559b8e6b70e0b23775e2708c1a50
Faye Rosenshein <rosefaye.a@gmail.com>
2023-04-05 12:01:59 -0600

    Change p tag

--- src/app/public-api/page.jsx
+++ src/app/public-api/page.jsx
@@ -4,8 +4,7 @@ export default function publicApi() {
 	return (
 		<>
 		<h1>Here is our free public api</h1>
-		<p>For all shelter GET requests: <Link href={"https://gaybe-safe-haven.herokuapp.com/api/v1/shelters"} >https://gaybe-safe-haven.herokuapp.com/api/v1/shelters</Link></p>
-		<p>For one shelter GET request, find the id of the shelter and create a unique url: https://gaybe-safe-haven.herokuapp.com/api/v1/shelters/:id</p>
+		<p>Base GET URL for all shelters: <Link href={"https://gaybe-safe-haven.herokuapp.com/api/v1/shelters"} >https://gaybe-safe-haven.herokuapp.com/api/v1/shelters</Link></p>
 		</>
 	)
 }
