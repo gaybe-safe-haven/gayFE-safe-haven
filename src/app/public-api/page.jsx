6147787c83e7aebac23d759eca31b65b4ee6c8bb
Faye Rosenshein <rosefaye.a@gmail.com>
2023-04-05 12:04:18 -0600

    Add line about api

--- src/app/public-api/page.jsx
+++ src/app/public-api/page.jsx
@@ -4,6 +4,7 @@ export default function publicApi() {
 	return (
 		<>
 		<h1>Here is our free public api</h1>
+		<p>Use this api to get a community created list of shelters</p>
 		<p>Base GET URL for all shelters: <Link href={"https://gaybe-safe-haven.herokuapp.com/api/v1/shelters"} >https://gaybe-safe-haven.herokuapp.com/api/v1/shelters</Link></p>
 		</>
 	)
