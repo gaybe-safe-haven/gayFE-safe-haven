332d46ede6ce63926677065032d0dfc30653c4b5
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-04-05 16:17:21 -0400

    Fix cypress tests to match new ids

--- cypress/e2e/add-shelter-form.cy.js
+++ cypress/e2e/add-shelter-form.cy.js
@@ -46,19 +46,18 @@ describe("User Flow: As a user, when I choose to add a shelter to the list, I am
 
   it("Should have a message that communicates a successful addition when the button is clicked", () => {
     cy.intercept('POST', 'https://gaybe-safe-haven.herokuapp.com/api/v1/shelters', {fixture: '../fixtures/postShelter200.json'} )
-    cy.get('form').within((form) => {
       cy.get('input[type=text][name="zip"]').type("02460")
       cy.get('input[type=text][name="phoneNumber"]').type("(123)456-7890")
       cy.get('input[type=text][name="websiteUrl"]')
       cy.get("button.Form_button__BbaEK").contains("Add Shelter").click()
-    })
+
     cy.get('.message').contains("Your submission was successful!").should("be.visible")
-    cy.get('.Form_receipt___kJJe > :nth-child(1)').contains('Thrive Youth Center')
-    cy.get('.Form_receipt___kJJe > :nth-child(2)').contains("1 Haven for Hope Way")
-    cy.get('.Form_receipt___kJJe > :nth-child(3)').contains('San Antonio')
-    cy.get('.Form_receipt___kJJe > :nth-child(3)').contains('TX')
-    cy.get('.Form_receipt___kJJe > :nth-child(3)').contains('78207')
-    cy.get('.Form_receipt___kJJe > :nth-child(4)').contains('312 234-1234')
+    cy.get('section > div > :nth-child(1)').contains('Thrive Youth Center')
+    cy.get('section > div > :nth-child(2)').contains("1 Haven for Hope Way")
+    cy.get('section > div > :nth-child(3)').contains('San Antonio')
+    cy.get('section > div > :nth-child(3)').contains('TX')
+    cy.get('section > div > :nth-child(3)').contains('78207')
+    cy.get('section > div > :nth-child(4)').contains('312 234-1234')
 
     cy.get('section > :nth-child(3)').click()
     .url().should("eq", "http://localhost:3000/list/1")
@@ -116,7 +115,7 @@ describe("User Flow: As a user, when I choose to add a shelter to the list, I am
       cy.get('input[type=text][name="websiteUrl"]').type('www.google.com')
       cy.get("button.Form_button__BbaEK").click()
       cy.get('.message').contains("Your submission was successful!").should("be.visible")
-      cy.get('.Form_receipt___kJJe > :nth-child(1)').contains('Thrive Youth Center')
+      cy.get('section > div > :nth-child(1)').contains('Thrive Youth Center')
 
       cy.get('section > :nth-child(4)').click()
       cy.get('form').should("be.visible")
