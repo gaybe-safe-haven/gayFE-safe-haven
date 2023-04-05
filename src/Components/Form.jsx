7e64567a26578ad557c20ced9e9f061338562953
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-04-05 10:19:41 -0400

    Replace state input with dropdown

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -5,10 +5,10 @@ import { useEffect, useState } from "react";
 import { checkSite, checkZip, checkPhone } from '../util'
 
 export default function Form() {
-  const states = [AL, AK, AZ, AR, AS, CA, CO, CT, DC, DE, FL, GA, GU, HI, ID, IL, IN, IA, KS, KY, LA, 
-    ME, MD, MA, MI, MN, MS, MO, MT, NE, NV, NH, NJ, NM, NY, NC, ND, MP, OH, OK, OR, PA, PR, RI, SC, SD, 
-    TN, TX, TT, UT,VT, VA, VI, WA, WV, WI, WY]
-    
+  const states = ['AL', 'AK', 'AZ', 'AR', 'AS', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 
+    'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PA', 'PR', 'RI', 'SC', 'SD', 
+    'TN', 'TX', 'TT', 'UT', 'VT', 'VA', 'VI', 'WA', 'WV', 'WI', 'WY']
+
   const [formData, setFormData] = useState(
     {
       name: "",
@@ -146,6 +146,12 @@ export default function Form() {
           onChange={(e) => handleChange(e)}
         />
       </div> */}
+      <div className={styles.inputContainer} >
+        <select name='state' onChange={(e) => {handleChange(e)}} required={true} value={formData.state}>
+          <option key='default' value=''>Select State:</option>
+          {states.map(state => <option key={state} value={state} >{state}</option>)}
+        </select>
+      </div>
 
       <div className={styles.inputContainer}>
         <input 
