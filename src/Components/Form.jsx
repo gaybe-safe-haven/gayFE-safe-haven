8645bfd38b1188555417f8cffdaf84fa201bcc48
Anna Peterson <reveragb@gmail.com>
2023-04-05 06:55:43 -0600

    Import proptypes

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -2,6 +2,7 @@
 import styles from "./Form.module.css";
 import { postData } from "../apiCalls";
 import { useState } from "react";
+import PropTypes from 'prop-types';
 
 export default function Form() {
   const [formData, setFormData] = useState(
