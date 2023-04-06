a6d4f6a38103fcec1330fc6b858dab22306f6a99
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-04-06 10:36:34 -0700

    Add more API page styling for additional media query

--- src/app/public-api/page.jsx
+++ src/app/public-api/page.jsx
@@ -7,15 +7,26 @@ import example from "../../../public/example.png"
 export default function publicApi() {
 	return (
 		<main className={styles.main}>
-			<h1>Here is our free public API</h1>
-			<p>Use this API to get a community created list of shelters</p>
-			<p><span className={styles.span}>GET</span> endpoint for all shelters: 
-				<Link href={"https://gaybe-safe-haven.herokuapp.com/api/v1/shelters"} >https://gaybe-safe-haven.herokuapp.com/api/v1/shelters</Link>
-			</p>
-			<p><span className={styles.span}>GET</span> endpoint for a single shelter: 
-				<Link href={"https://gaybe-safe-haven.herokuapp.com/api/v1/shelters"} >https://gaybe-safe-haven.herokuapp.com/api/v1/shelters/:id</Link>
-			</p>
-			<Image className={imageStyles.example} src={example} alt="API get request example" />
+			<div className={styles.wrapperAPI}>
+				<div className={styles.leftWrapperAPI}>
+					<h2>Here is our free public API</h2>
+					<p>Use this API to get a community created list of shelters</p>
+					<hr />
+					<p><span className={styles.span}>GET</span> endpoint for all shelters: 
+					</p>
+					<p>
+						<Link href={"https://gaybe-safe-haven.herokuapp.com/api/v1/shelters"} >https://gaybe-safe-haven.herokuapp.com/api/v1/shelters</Link>
+					</p>
+					<hr />
+					<p><span className={styles.span}>GET</span> endpoint for a single shelter: 
+					</p>
+					<p>
+						<Link href={"https://gaybe-safe-haven.herokuapp.com/api/v1/shelters"} >https://gaybe-safe-haven.herokuapp.com/api/v1/shelters/:id</Link>
+					</p>
+					<hr />
+				</div>
+				<Image className={imageStyles.example} src={example} alt="API get request example" />
+			</div>
 		</main>
 	)
 }
