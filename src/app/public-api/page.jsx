7102ae08f482887b6c36ab4fa52bdf65a6231fb7
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-04-06 09:54:16 -0700

    Add styling to API page with change to main tag and import of page module

--- src/app/public-api/page.jsx
+++ src/app/public-api/page.jsx
@@ -1,11 +1,13 @@
 import Link from "next/link"
+import styles from "../page.module.css"
 
 export default function publicApi() {
 	return (
-		<>
-		<h1>Here is our free public api</h1>
-		<p>Use this api to get a community created list of shelters</p>
-		<p>Base GET URL for all shelters: <Link href={"https://gaybe-safe-haven.herokuapp.com/api/v1/shelters"} >https://gaybe-safe-haven.herokuapp.com/api/v1/shelters</Link></p>
-		</>
+		<main className={styles.main}>
+			<h1>Here is our free public API</h1>
+			<p>Use this API to get a community created list of shelters</p>
+			<p>GET endpoint for all shelters: <Link href={"https://gaybe-safe-haven.herokuapp.com/api/v1/shelters"} >https://gaybe-safe-haven.herokuapp.com/api/v1/shelters</Link></p>
+			<p>GET endpoint for a single shelter: <Link href={"https://gaybe-safe-haven.herokuapp.com/api/v1/shelters"} >https://gaybe-safe-haven.herokuapp.com/api/v1/shelters/:id</Link></p>
+		</main>
 	)
 }
